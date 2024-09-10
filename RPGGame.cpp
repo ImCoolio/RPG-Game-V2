fa79154748141f7ceb573295b4c53f21c83a32f7
coolius <levelcoolio767@gmail.com>
2024-09-09 23:47:51 -0400

    fixed clear ACTUALLY this time and it looks much nicer.

--- RPGGame.cpp
+++ RPGGame.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
-#include <windows.h>
 #include <limits>
-using namespace std;
 
 void menu();
+void clear();
 
 int main() {
-  cout << "\033c";
-  cout << "Welcome to the (TBD) RPG! v0.01\n\n";
-  cout << "This is just a slight project I felt like developing... don't expect it to go anywhere.\n\n";
-  cout << "PRESS ENTER TO START\n\n";
+  clear();
+  std::cout << "Welcome to the (TBD) RPG! v0.01\n\n";
+  std::cout << "This is just a slight project I felt like developing... don't expect it to go anywhere.\n\n";
+  std::cout << "PRESS ENTER TO START\n\n";
   getchar();
   menu();
   return 0;
@@ -18,33 +17,43 @@ int main() {
 
 void menu() {
   int x;
-  cout << "\033c";
+  clear();
   while (true) {
-    cout << "1 - Play\n2 - Options\n3 - Quit";
-    if (cin >> x) {
+    std::cout << "1 - Play\n2 - Options\n3 - Quit";
+    if (std::cin >> x) {
       switch (x) {
         case 1:
-          cout << "\033c";
-          cout << "1 Test\n\n";
+          clear();
+          std::cout << "1 Test\n\n";
           break;
         case 2:
-          cout << "\033c";
-          cout << "2 Test\n\n";
+          clear();
+          std::cout << "2 Test\n\n";
           break;
         case 3:
-          cout << "\033c";
-          cout << "3 Test\n\n";
+          clear();
+          std::cout << "3 Test\n\n";
           break;
         default:
-          cout << "\033c";
-          cout << "Invalid input. Please retry.\n\n";
+          clear();
+          std::cout << "Invalid input. Please retry.\n\n";
           break;
       }
     } else {
-      cout << "\033c";
-      cout << "Invalid input, be sure you are inputting an integer.\n\n";
-      cin.clear();
-      while (cin.get() != '\n');
+      clear();
+      std::cout << "Invalid input, be sure you are inputting an integer.\n\n";
+      std::cin.clear();
+      while (std::cin.get() != '\n');
     }
   }
+}
+
+void clear() {
+  #if defined(WIN32) && !defined(__linux__)
+    system("cls");
+  #elif defined(__linux__) && !defined(WIN32)
+    system("clear");
+  #else
+    std::cout << "Why are you running this on apple...? \n\n";
+  #endif
 }
