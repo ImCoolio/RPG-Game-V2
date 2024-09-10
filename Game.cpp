be9cf20d341bb1fd77f28940cf3858ddf4e1b9fa
coolio <levelcoolio767@gmail.com>
2024-09-09 22:21:01 -0400

    Clearing terminal before game start

--- Game.cpp
+++ Game.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
+void clear();
+
 int main() {
-  cout << "Welcome to the (TBD) RPG!\n\n";
+  clear();
+  cout << "Welcome to the (TBD) RPG! v0.01\n\n";
+  cout << "This is just a slight project I felt like developing... don't expect it to go anywhere.\n";
+  cout << "PRESS ENTER TO START";
   return 0;
+}
+
+void clear() {
+  #if defined _WIN32
+    system("cls");
+  #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+  #elif defined (__APPLE__)
+    system("clear");
+  #endif
 }
