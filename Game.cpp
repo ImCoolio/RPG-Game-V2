6ad6758138a2ce5dac434a1a596310338e52b062
coolio <levelcoolio767@gmail.com>
2024-09-09 22:36:49 -0400

    Removed clear function, added 1 line that replaced it lol.

--- Game.cpp
+++ Game.cpp
@@ -6,19 +6,10 @@ using namespace std;
 void clear();
 
 int main() {
-  clear();
+  cout << "\033c";
   cout << "Welcome to the (TBD) RPG! v0.01\n\n";
-  cout << "This is just a slight project I felt like developing... don't expect it to go anywhere.\n";
+  cout << "This is just a slight project I felt like developing... don't expect it to go anywhere.\n\n";
   cout << "PRESS ENTER TO START";
+  getchar();
   return 0;
-}
-
-void clear() {
-  #if defined _WIN32
-    system("cls");
-  #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-    std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
-  #elif defined (__APPLE__)
-    system("clear");
-  #endif
 }
