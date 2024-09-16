c5e80c59030589180f55fdd0131cda1a1c610048
coolius <levelcoolio767@gmail.com>
2024-09-16 16:11:34 -0400

    Created random handlers

---
+++ src/Random.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Random.h"
+
+namespace rand {
+    int randSpot() {
+        // research procedural generation to generate a balanced map
+        return 0;
+    }
+}
---
+++ src/Random.h
@@ -0,0 +1,10 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+#include <random>
+
+namespace rand {
+    int randSpot();
+
+}
+
+#endif
